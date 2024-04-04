12ba09bf099b087351cdab2fb6f246ffbdd40c4d
Ebrahim Hosseini <sehosseini@me.com>
2024-04-05 00:10:34 +0330

    fisrt step to buy a ticket flow

--- main.go
+++ main.go
@@ -8,4 +8,26 @@ func main() {
 	// Welcome message
 	const companyName = "Flight Booking"
 	fmt.Printf("Welecom to %v.\n", companyName)
+
+	totalTickets := 100
+	var remainingTickets = totalTickets
+
+	var name string
+	var family string
+	var ticket int
+
+	fmt.Println("Enter name: ")
+	fmt.Scan(&name)
+
+	fmt.Println("Enter name: ")
+	fmt.Scan(&family)
+
+	fmt.Println("How many ticket(s) do you want to buy?")
+	fmt.Scan(&ticket)
+
+	remainingTickets -= ticket
+
+	fmt.Printf("Thanks, %v %v you buy %v ticke(s).\n", name, family, ticket)
+	fmt.Printf("The %v ticket(s) is remaining.\n", remainingTickets)
+
 }
