15c27af109982e047d9e1fbcd4bbd14599c8ef42
Ebrahim Hosseini <sehosseini@me.com>
2024-04-05 02:09:55 +0330

    refactor for loop

--- main.go
+++ main.go
@@ -16,29 +16,26 @@ func main() {
 	var family string
 	var ticket int
 
-	for {
-		if remainingTickets > 0 {
-			fmt.Println("\nEnter your firstname: ")
-			fmt.Scan(&name)
+	for remainingTickets > 0 {
+		fmt.Println("\nEnter your firstname: ")
+		fmt.Scan(&name)
 
-			fmt.Println("Enter your lastname: ")
-			fmt.Scan(&family)
+		fmt.Println("Enter your lastname: ")
+		fmt.Scan(&family)
 
-			fmt.Println("How many ticket(s) do you want to buy?")
-			fmt.Scan(&ticket)
+		fmt.Println("How many ticket(s) do you want to buy?")
+		fmt.Scan(&ticket)
 
-			if remainingTickets >= ticket {
-				remainingTickets -= ticket
+		if remainingTickets >= ticket {
+			remainingTickets -= ticket
 
-				fmt.Printf("Thanks, %v %v buy %v ticke(s).\n", name, family, ticket)
-				fmt.Printf("The %v ticket(s) is remaining.\n", remainingTickets)
-			} else {
-				fmt.Printf("Your request is more than remainig. The remaining tickets are %v.\n", remainingTickets)
-			}
-
+			fmt.Printf("Thanks, %v %v buy %v ticke(s).\n", name, family, ticket)
+			fmt.Printf("The %v ticket(s) is remaining.\n", remainingTickets)
 		} else {
-			fmt.Printf("The tickets sold out.\n")
-			break
+			fmt.Printf("Your request is more than remainig. The remaining tickets are %v.\n", remainingTickets)
 		}
+
 	}
+
+	fmt.Printf("The tickets sold out.\n")
 }
