70ea84a972865ec4fc86094704f23d63fd6f0ecb
Ebrahim Hosseini <sehosseini@me.com>
2024-04-05 00:49:56 +0330

    now you can sell tickets

--- main.go
+++ main.go
@@ -16,18 +16,30 @@ func main() {
 	var family string
 	var ticket int
 
-	fmt.Println("Enter name: ")
-	fmt.Scan(&name)
+	for {
+		if remainingTickets > 0 {
+			fmt.Println("\nEnter your firstname: ")
+			fmt.Scan(&name)
 
-	fmt.Println("Enter name: ")
-	fmt.Scan(&family)
+			fmt.Println("Enter your lastname: ")
+			fmt.Scan(&family)
 
-	fmt.Println("How many ticket(s) do you want to buy?")
-	fmt.Scan(&ticket)
+			fmt.Println("How many ticket(s) do you want to buy?")
+			fmt.Scan(&ticket)
 
-	remainingTickets -= ticket
+			if remainingTickets >= ticket {
+				remainingTickets -= ticket
 
-	fmt.Printf("Thanks, %v %v you buy %v ticke(s).\n", name, family, ticket)
-	fmt.Printf("The %v ticket(s) is remaining.\n", remainingTickets)
+				fmt.Printf("Thanks, %v %v buy %v ticke(s).\n", name, family, ticket)
+				fmt.Printf("The %v ticket(s) is remaining.\n", remainingTickets)
+			} else {
+				fmt.Printf("Your request is more than remainig. The remaining tickets are %v.\n", remainingTickets)
+			}
+
+		} else {
+			fmt.Printf("The tickets sold out.\n")
+			break
+		}
+	}
 
 }
